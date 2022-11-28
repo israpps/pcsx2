46a886a44a8f5c7f3bd50882e7701dc7beee0c28
Matías Israelson <57065102+israpps@users.noreply.github.com>
2022-11-28 11:04:59 -0300

    d

--- mscompile.cmd
+++ mscompile.cmd
@@ -2,7 +2,7 @@
 REM PCSX2 - PS2 Emulator for PCs
 REM Copyright (C) 2002-2015  PCSX2 Dev Team
 REM
-REM PCSX2 is free software: you can redistribute it and/or modify it under the terms
+REM PCSX2 is free software: you can redistribute it and/or modify it unsadder the terms
 REM of the GNU Lesser General Public License as published by the Free Software Found-
 REM ation, either version 3 of the License, or (at your option) any later version.
 REM
